34b69bb64af9333977dd1cf9d5cda5aff2e54283
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2025-09-25 01:16:11 +0530

    Add CrossOrigin annotation to TrackController

--- src/main/java/com/beatflow/backend/controller/TrackController.java
+++ src/main/java/com/beatflow/backend/controller/TrackController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tracks")
+@CrossOrigin(origins = "*")
 public class TrackController {
 
     private final TrackService trackService;
